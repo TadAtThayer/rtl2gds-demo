c6fa1e1c4e333ca1a680fc22fb57af1983250c21
Tad Truex (d66317d) <tad@dartmouth.edu>
2026-05-06 10:06:19 -0400

    wip

--- labs/genus/LAB2/run.tcl
+++ labs/genus/LAB2/run.tcl
@@ -387,7 +387,7 @@ report_dp > $_REPORTS_PATH/${DESIGN}_datapath_incr.rpt
 report_messages > $_REPORTS_PATH/${DESIGN}_messages.rpt
 write_snapshot -outdir $_REPORTS_PATH -tag final
 report_summary -directory $_REPORTS_PATH
-write_hdl  > ${_OUTPUTS_PATH}/${DESIGN}_m.v
+## write_hdl  > ${_OUTPUTS_PATH}/${DESIGN}_m.v
 ## write_script > ${_OUTPUTS_PATH}/${DESIGN}_m.script
 write_sdc > ${_OUTPUTS_PATH}/${DESIGN}_m.sdc
 
