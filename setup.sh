9ed37fa8396b9ffc94a91938391d47cb58c47a30
Tad Truex (d66317d) <tad@dartmouth.edu>
2026-05-06 08:46:12 -0400

    wip

--- setup.sh
+++ setup.sh
@@ -7,15 +7,24 @@ git submodule update --init --recursive --remote
 module unload cadence-SPECTRE cadence-IC cadence-PVS cadence-DDI cadence-XCELIUM
 
 # Grab the correct versions of the tools
-module load cadence-XCELIUM/22.09-2 cadence-DDI/23.1
+module load cadence-XCELIUM/22.09-2 cadence-DDI/23.1 cadence-IC/23.100
 
 export RAK=/thayerfs/courses/26spring/engs084/workspace/rak/Genus_CUI_RAK
+export RAK2=/thayerfs/courses/26spring/engs084/workspace/rak/RAK_floorplanning_22.1
 
+mkdir -p labs/genus
 ln -sf $RAK/LEF labs/genus/
 ln -sf $RAK/LIB labs/genus/
 ln -sf $RAK/RTL labs/genus/
 ln -sf $RAK/constraints labs/genus/
+cp -r $RAK/LAB* labs/genus/
 
+mkdir -p labs/innovus
+ln -sf $RAK2/DATA labs/innovus/
+ln -sf $RAK2/DBS labs/innovus/
+ln -sf $RAK2/DOCs labs/innovus/
+ln -sf $RAK2/libs labs/innovus/
+ln -sf $RAK2/SCRIPTS labs/innovus/
 
 
 
